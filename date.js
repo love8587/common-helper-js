1e7867bdf8c24524c19666ab32c95f8a78cb9592
Steve <steve@itsdcode.com>
2018-09-12 14:35:09 +0900

    test

--- date.js
+++ date.js
@@ -3,49 +3,47 @@ const parseFormat = require('moment-parseformat')
 const API_DATE_FORMAT = 'YYYY-MM-DDTHH:mm:ss.SSSZZ'
 const DB_DATE_FORMAT = 'YYYY-MM-DD HH:mm:ss'
 
-var _convertDateTime = function (iso8601, format) {
+var _convertDateTime = function (iso8601, format, utcOffset = '+00:00') {
   var converted = moment(iso8601, [moment.ISO_8601, parseFormat(iso8601)])
 
-  return (converted.isValid()) ? converted.utcOffset('+09:00')
-    // .utc()
-    .format(format) : moment.utc()
-    .format(format)
+  return (converted.isValid())
+    ? converted.utcOffset(utcOffset).format(format) : moment().utcOffset(utcOffset).format(format)
 }
 
-var nowForDb = function () {
-  return moment().utcOffset('+09:00')
+var nowForDb = function (utcOffset = '+00:00') {
+  return moment().utcOffset(utcOffset)
     // .utc()
     .format(DB_DATE_FORMAT)
 }
 
-var nowForApi = function () {
-  return moment().utcOffset('+09:00')
+var nowForApi = function (utcOffset = '+00:00') {
+  return moment().utcOffset(utcOffset)
     // .utc()
     .format(API_DATE_FORMAT)
 }
 
-var toDateTimeForDb = function (iso8601) {
+var toDateTimeForDb = function (iso8601, utcOffset = '+00:00') {
   if (!iso8601) {
     return null
   }
 
   if (iso8601 === '0000-00-00 00:00:00') {
-    iso8601 = nowForDb()
+    iso8601 = nowForDb(utcOffset)
   }
 
-  return _convertDateTime(iso8601, DB_DATE_FORMAT)
+  return _convertDateTime(iso8601, DB_DATE_FORMAT, utcOffset)
 }
 
-var toDateTimeForApi = function (iso8601) {
+var toDateTimeForApi = function (iso8601, utcOffset = '+00:00') {
   if (!iso8601) {
     return null
   }
 
   if (iso8601 === '0000-00-00 00:00:00') {
-    iso8601 = nowForApi()
+    iso8601 = nowForApi(utcOffset)
   }
 
-  return _convertDateTime(iso8601, API_DATE_FORMAT)
+  return _convertDateTime(iso8601, API_DATE_FORMAT, utcOffset)
 }
 
 module.exports = {
